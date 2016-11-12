15b066233bde272d194ba171b59ad459a6df9366
Chetan Bansiwal <chetanbansiwal@live.co.uk>
2016-11-12 12:49:18 +0530

    Exit problem resolved and gitignore changed

--- app/src/main/java/in/tagbin/mitohealthapp/activity/BinderActivity.java
+++ app/src/main/java/in/tagbin/mitohealthapp/activity/BinderActivity.java
@@ -297,7 +297,10 @@ public class BinderActivity extends AppCompatActivity implements View.OnClickLis
         dialog.setPositiveButton("yes", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                finish();
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_HOME);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
             }
         });
         dialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
